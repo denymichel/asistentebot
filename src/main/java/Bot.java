46a3cedbde53f164a43d41b70e2be21752bc654d
geovanna <yovanita282.gcg65@gmail.com>
2019-10-03 20:25:49 -0300

    creando la clase Bot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,2 +1,29 @@
-public class Bot {
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class But extends TelegramLongPollingBot {
+    public static void main (String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            TelegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return "";
+    }
+
+    public String getBotToken() {
+        return "965898434:AAFYisxZkAsAWykdChxs9DNy1ceCADAmogo";
+    }
 }
