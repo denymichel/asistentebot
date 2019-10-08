758f14f142d12d5b9ba6446d8b61e630440a1284
alvaromoller <47613105+alvaromoller@users.noreply.github.com>
2019-10-08 10:53:48 -0400

    cambiando codigo de MainBot

--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -1,35 +1,51 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class But extends TelegramLongPollingBot {
+
+public class MainBot extends TelegramLongPollingBot{
+
     public static void main (String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+    try { telegramBotsApi.registerBot(new MainBot());
+    } catch (TelegramApiRequestException e) {
+        e.printStackTrace();
+    }
+  }
+
+
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
         try {
-            TelegramBotsApi.registerBot(new Bot());
-        } catch (TelegramApiRequestException e) {
+            sendMessage(sendMessage);
+        }catch (TelegramApiException e){
             e.printStackTrace();
         }
     }
 
     public void onUpdateReceived(Update update) {
-        Model model = new Model();
+    //  Model model = new Model();
         Message message = update.getMessage();
         if (message != null && message.hasText()) {
             switch (message.getText()) {
-                case "/hola":
+                case "/help":
                     sendMsg(message, "hola soy el asistente automatico");
                     break;
                 case "/ayuda":
                     sendMsg(message, "En que te puedo ayudar?");
                     break;
-                case "/realizar reserva":
+                default:
+       /*         case "/realizar reserva":
                     sendMsg(message, "Escoge un horario para la reserva");
                     break;
                 default:
@@ -38,23 +54,21 @@ public class But extends TelegramLongPollingBot {
                     } catch (IOException e) {
                         sendMsg(message, " puedes realizar reservas, ver horarios y especialidades");
                     }
-
+        */
             }
         }
 
     }
 
-
-
-
-
     public String getBotUsername() {
-        return "";
+        return "GeoDanAlvBot";
     }
 
     public String getBotToken() {
-        return "965898434:AAFYisxZkAsAWykdChxs9DNy1ceCADAmogo";
+        return "845095054:AAHnNtOMHFcanYBH4v7N2bIk8ij3mA9THFc";
     }
 }
+
+
 
 
