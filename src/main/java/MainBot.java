7a837270d88c12a35793a5a3a46957966414c6a9
pokeyova <yovanita282.gcg65@gmail.com>
2019-10-17 19:38:27 -0400

    funcionalidad de botones

--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -58,7 +58,7 @@ public class MainBot extends TelegramLongPollingBot{
                             "/Pediatria         " +
                             "/Traumatologia     ");
                     break;
-                    
+
             }
         }
 
